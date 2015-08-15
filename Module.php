e5c82ddd1083854c3fcf3cb83fdac26fa89cd32c
Paroklo <patroklo@gmail.com>
2015-08-15 17:09:41 +0200

    Changes

--- Module.php
+++ Module.php
@@ -17,7 +17,7 @@ class Module extends \atuin\skeleton\Module
 
     protected static $_id = 'installation';
 
-    protected static $_version = '0';
+    protected static $_version = '0.0.1';
 
     public $is_core_module = 1;
 
